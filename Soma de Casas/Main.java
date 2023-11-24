953229169b21c6a659f3724a0b098fb7050aec7a
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2023-11-24 11:09:39 -0300

    .

--- Soma de Casas/Main.java
+++ Soma de Casas/Main.java
@@ -1,6 +1,29 @@
+/*
+Joãozinho mora em uma rua que tem N casas. Marquinhos é o melhor amigo dele, mas sempre gosta de pregar peças em Joãozinho. 
+Desta vez, ele pegou os dois brinquedos prediletos de Joãozinho e os escondeu em duas casas distintas da rua. 
+Em compensação, Marquinhos deu uma dica importante para Joãozinho:
+
+A soma dos números das casas em que escondi teus brinquedos é igual a K. Além disso, escolhi as casas de tal forma que 
+não existe outro par de casas cuja soma tenha esse mesmo valor.
+
+Sabendo disto, encontre qual é o par de casas em que se encontram os brinquedos de Joãozinho. Para auxiliar seu amigo, 
+Marquinhos entregou a Joãozinho uma lista com o número das casas já em ordem crescente (isto é, do menor para o maior número).
+
+Entrada
+A primeira primeira linha da entrada contém um número inteiro N, que representa o número de casas que existem na rua. Cada uma 
+das N linhas seguintes contém um número inteiro, representando o número de uma casa. Note que esses N números estão 
+ordenados, do menor para o maior.
+
+A última linha da entrada contém um inteiro K, que é a soma dos números das duas casas onde os brinquedos estão escondidos.
+
+Saída
+Se programa deve imprimir uma única linha, contendo dois inteiros, A e B, A<B, que representam os números das casas em que 
+estão escondidos os brinquedos. Os dois números devem ser separados por um espaço em branco.
+*/
+
 import java.util.*;
 
-public class Q2 {
+public class Main {
     public static void operacoes (int [] listaCasas, int quantidadeCasas, int somaNumeroCasas) {
         int somaTemp;
 
