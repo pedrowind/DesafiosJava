4bea797a15f58c145cbfeaaf40df845e4886454c
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2023-11-24 11:07:00 -0300

    Update and rename Q4.java to Main.java

--- Fuga com Helicóptero/Main.java
+++ Fuga com Helicóptero/Main.java
@@ -1,5 +1,27 @@
+/*
+Um fugitivo, um helicóptero e um policial estão em posições distintas numa pista circular, exatamente como a mostrada na figura ao lado, 
+com dezesseis posições numeradas de 0 a 15 em direção anti-horária.
+
+O helicóptero e o policial ficam sempre parados. O objetivo do fugitivo é chegar no helicóptero sem passar pelo policial antes, claro. 
+Ele pode decidir correr na direção horária, ou na direção anti-horária.
+
+Neste problema, dadas as posições do helicóptero, do policial e do fugitivo, e a direção em que o fugitivo decide correr, seu programa 
+deve dizer se ele vai ou não conseguir fugir! Na figura, se o fugitivo decidir correr na direção horária, ele consegue fugir; se decidir 
+correr na direção anti-horária, ele vai ser preso antes de chegar no helicóptero!
+
+Entrada
+A entrada consiste de uma linha com quatro inteiros: H, P, F e D, representando, respectivamente, as posições do helicóptero, do policial
+e do fugitivo, e a direção em que o fugitivo corre, -1 para horário e 1 para anti-horário.
+
+Saída
+Seu programa deve imprimir uma linha contendo o caractere "S" se o fugitivo consegue fugir, ou "N" caso contrário
+
+Restrições
+Os inteiros H, P e F são distintos e estão entre 0 e 15, inclusive
+*/
+
 import java.util.*;
-public class Q4 {
+public class Main {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
@@ -38,13 +60,3 @@ public class Q4 {
         }
     }
 }
-/*
-        System.out.println("Insira a posição do Helicoptero: ");
-                int posicaoHelicoptero = sc.nextInt();
-                System.out.println("Insira a posição do Policial: ");
-                int posicaoPolicial = sc.nextInt();
-                System.out.println("Insira a posição do Fugitivo: ");
-                int posicaoFugitivo = sc.nextInt();
-                System.out.println("Insira a direção que será percorrida: (-1 horário, 1 anti-horário) ");
-                int sentido = sc.nextInt();
-*/
